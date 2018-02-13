e375e38fb7a2787bf74e0942798b63870c52b622
MeanDean7 <35509928+MeanDean7@users.noreply.github.com>
2018-02-13 18:50:03 -0500

    new week 5 arraylist

--- Arraylist.java
+++ Arraylist.java
@@ -1,18 +1,23 @@
+package classWork;
+
 import java.util.ArrayList;
 
 public class Arraylist {
-	public static void main(String[] args) {
-		ArrayList<String> teachers = new ArrayList<String>();
-		
-		teachers.add("Anthony");
-		teachers.add("Paul");
-		teachers.add("John");
-		teachers.add("Martin");
-		
-		System.out.println( teachers.get(0) );
-		System.out.println( teachers.get(1) );
-		System.out.println( teachers.get(2) );
-		System.out.println( teachers.get(3) );
+	public static void removeFirst(ArrayList<Integer> list) { //This method removes the first number in the array
+		list.remove(0);
 	}
-
+	public static void main(String[] args) { 
+		ArrayList<Integer> numbers = new ArrayList<Integer>();//we are making an array of numbers
+		numbers.add(4);//we add these numbers
+		numbers.add(3);
+		numbers.add(7);
+		numbers.add(3);
+		
+		System.out.println(numbers);//Print the numbers
+		
+		removeFirst(numbers); //Remove first number
+		
+		System.out.println(numbers); //This time 4 is missing
+	}
 }
+
